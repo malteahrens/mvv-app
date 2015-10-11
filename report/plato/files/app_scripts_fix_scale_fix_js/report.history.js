e9457a993541e7a536b86fe3c8a796bcc5a4d37f
deploy.sh <xyz@test.de>
2015-10-11 19:04:50 +0000

    publish: cleanup
    generated from commit ce48a4f345c977277e7db76b91d56789818ed078

---
+++ report/plato/files/app_scripts_fix_scale_fix_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Sun, 11 Oct 2015 19:04:26 GMT","sloc":17,"lloc":11,"functions":1,"deliveredBugs":0.12921478568435765,"maintainability":76.5778933262416,"lintErrors":[],"difficulty":10.75}]
---
+++ report/plato/files/app_scripts_lib_cordova_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Sun, 11 Oct 2015 19:04:26 GMT","sloc":224,"lloc":127,"functions":20,"deliveredBugs":1.649082750331732,"maintainability":73.22767100383267,"lintErrors":3,"difficulty":38.7}]
