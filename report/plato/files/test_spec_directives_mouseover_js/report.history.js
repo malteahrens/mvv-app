37e094c240c225f6acbc6756094dbf7ad709e4de
deploy.sh <xyz@test.de>
2015-10-11 19:41:30 +0000

    publish: cleanup
    generated from commit 4c30c847f6991149945d1f961e56d987f9c87549

--- report/plato/files/test_spec_directives_mouseover_js/report.history.js
+++ report/plato/files/test_spec_directives_mouseover_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 11 Oct 2015 19:23:32 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
+__history = [{"date":"Sun, 11 Oct 2015 19:41:29 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
