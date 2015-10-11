9a1573e9cd214b47d821e96ee7aa85295fd528c2
malteahrens <malteahrens@gmail.com>
2015-10-11 22:22:51 +0200

    Create github.sh

---
+++ deploy/scripts/github.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -o errexit #abort if any command fails
+
+#if no user identity is already set in the current git environment, use this:
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+#repository to deploy to. must be readable and writable.
+repo=https://malteahrens:${GH_TOKEN}@github.com/malteahrens/mvv-app
+
+# clear and re-create the out directory
+#rm -rf release || exit 0;
+#mkdir release;
+
+git remote set-url origin $repo
+git status
+git add release
+git commit -m "Android release for SDK 22 [ci skip]"
+git push
