40172eddbfd9ba2b774c69e36120d7d953cc7ac5
blibla <blub@github.com>
2015-10-11 19:41:32 +0000

    Android App

---
+++ mvv-app/www/dist/report/plato/files/app_scripts_controllers_main_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Sun, 11 Oct 2015 19:41:03 GMT","sloc":28,"lloc":9,"functions":2,"deliveredBugs":0.08813484023772274,"maintainability":75.75840956065119,"lintErrors":3,"difficulty":5.5}]
