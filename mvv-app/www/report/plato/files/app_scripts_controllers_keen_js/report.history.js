9916dd5657463c942e4fd1e251b21d27290a473e
blibla <blub@github.com>
2015-10-12 16:56:31 +0000

    Android App

---
+++ mvv-app/www/report/plato/files/app_scripts_controllers_keen_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Mon, 12 Oct 2015 16:56:04 GMT","sloc":19,"lloc":11,"functions":2,"deliveredBugs":0.12245164198035031,"maintainability":71.91641535461339,"lintErrors":[],"difficulty":6.206896551724139}]
---
+++ mvv-app/www/report/plato/files/test_karma_conf_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Mon, 12 Oct 2015 16:56:27 GMT","sloc":77,"lloc":14,"functions":1,"deliveredBugs":0.14559225044393506,"maintainability":61.524977712198194,"lintErrors":[],"difficulty":4.295454545454546}]
