0f616e10a47a6b796e250bff75dfb521cf54549c
deploy.sh <xyz@test.de>
2015-10-12 16:56:28 +0000

    publish: cleanup
    generated from commit b9de34b667fd90d2eeb29865706a2ef0b2b73582

--- report/plato/files/app_scripts_lib_cordova_js/report.history.js
+++ report/plato/files/app_scripts_lib_cordova_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 12 Oct 2015 16:48:21 GMT","sloc":224,"lloc":127,"functions":20,"deliveredBugs":1.649082750331732,"maintainability":73.22767100383267,"lintErrors":3,"difficulty":38.7}]
+__history = [{"date":"Mon, 12 Oct 2015 16:56:05 GMT","sloc":224,"lloc":127,"functions":20,"deliveredBugs":1.649082750331732,"maintainability":73.22767100383267,"lintErrors":3,"difficulty":38.7}]
