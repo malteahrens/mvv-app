9b875bb3b5972ac6f9e0cbda82dd527cd05d0aa9
deploy.sh <xyz@test.de>
2015-10-12 16:48:40 +0000

    publish: cleanup
    generated from commit f7d5352fba6745ccc52ebf24c5de05a97fad0d34

--- report/plato/files/app_scripts_controllers_keen_js/report.history.js
+++ report/plato/files/app_scripts_controllers_keen_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 12 Oct 2015 16:26:06 GMT","sloc":19,"lloc":11,"functions":2,"deliveredBugs":0.12245164198035031,"maintainability":71.91641535461339,"lintErrors":[],"difficulty":6.206896551724139}]
+__history = [{"date":"Mon, 12 Oct 2015 16:48:21 GMT","sloc":19,"lloc":11,"functions":2,"deliveredBugs":0.12245164198035031,"maintainability":71.91641535461339,"lintErrors":[],"difficulty":6.206896551724139}]
--- report/plato/files/app_scripts_controllers_nvd3_js/report.history.js
+++ report/plato/files/app_scripts_controllers_nvd3_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 12 Oct 2015 16:26:07 GMT","sloc":7,"lloc":4,"functions":1,"deliveredBugs":1.3474248171689054,"maintainability":70.04252205391006,"lintErrors":[],"difficulty":5.983739837398374}]
+__history = [{"date":"Mon, 12 Oct 2015 16:48:21 GMT","sloc":7,"lloc":4,"functions":1,"deliveredBugs":1.3474248171689054,"maintainability":70.04252205391006,"lintErrors":[],"difficulty":5.983739837398374}]
