6082c31b486616104b810f3c323d8d844d47baaf
deploy.sh <xyz@test.de>
2015-10-12 17:14:59 +0000

    publish: cleanup
    generated from commit 53738de75bc8fd347655c603f7b31ba679efe34b

--- report/plato/files/app_scripts_fix_scale_fix_js/report.history.js
+++ report/plato/files/app_scripts_fix_scale_fix_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 12 Oct 2015 16:56:05 GMT","sloc":17,"lloc":11,"functions":1,"deliveredBugs":0.12921478568435765,"maintainability":76.5778933262416,"lintErrors":[],"difficulty":10.75}]
+__history = [{"date":"Mon, 12 Oct 2015 17:14:40 GMT","sloc":17,"lloc":11,"functions":1,"deliveredBugs":0.12921478568435765,"maintainability":76.5778933262416,"lintErrors":[],"difficulty":10.75}]
