06f439c5d1bf7eea5550ecca425fe823934d5dab
blibla <blub@github.com>
2015-10-13 17:18:12 +0000

    Android App

--- mvv-app/www/report/plato/files/test_karma_conf_js/report.history.js
+++ mvv-app/www/report/plato/files/test_karma_conf_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 12 Oct 2015 21:06:41 GMT","sloc":77,"lloc":14,"functions":1,"deliveredBugs":0.14559225044393506,"maintainability":61.524977712198194,"lintErrors":[],"difficulty":4.295454545454546}]
+__history = [{"date":"Tue, 13 Oct 2015 17:18:07 GMT","sloc":77,"lloc":14,"functions":1,"deliveredBugs":0.14559225044393506,"maintainability":61.524977712198194,"lintErrors":[],"difficulty":4.295454545454546}]
--- mvv-app/www/report/plato/files/test_spec_controllers_about_js/report.history.js
+++ mvv-app/www/report/plato/files/test_spec_controllers_about_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 12 Oct 2015 21:06:41 GMT","sloc":22,"lloc":11,"functions":3,"deliveredBugs":0.08744365791229632,"maintainability":79.87329168300973,"lintErrors":8,"difficulty":5.090909090909091}]
+__history = [{"date":"Tue, 13 Oct 2015 17:18:07 GMT","sloc":22,"lloc":11,"functions":3,"deliveredBugs":0.08744365791229632,"maintainability":79.87329168300973,"lintErrors":8,"difficulty":5.090909090909091}]
