01ab79ff6a6983061f32ba219f750184fd04e5a2
deploy.sh <xyz@test.de>
2015-10-13 20:49:28 +0000

    publish: new statistics
    generated from commit 00c6257040323dd818c62ad4539ee3b8b42b8cd3

--- report/plato/files/app_scripts_lib_jsts_min_js/report.history.js
+++ report/plato/files/app_scripts_lib_jsts_min_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 13 Oct 2015 17:17:43 GMT","sloc":1690,"lloc":14915,"functions":2253,"deliveredBugs":470.31785076585487,"maintainability":67.61977572547038,"lintErrors":51,"difficulty":493.0123605667772}]
+__history = [{"date":"Tue, 13 Oct 2015 20:49:08 GMT","sloc":1690,"lloc":14915,"functions":2253,"deliveredBugs":470.31785076585487,"maintainability":67.61977572547038,"lintErrors":51,"difficulty":493.0123605667772}]
