aadc354974fcb69f96cccce3812f4d71783bbd85
deploy.sh <xyz@test.de>
2015-10-14 22:12:47 +0000

    publish: new statistics
    generated from commit b9c0f2cc3df5c4bf7d323949cc9db12b1c0efcd3

--- report/plato/report.history.js
+++ report/plato/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 14 Oct 2015 21:11:06 GMT","total":{"sloc":25322,"maintainability":1714.4109594902538},"average":{"sloc":1100,"maintainability":"74.54"}}]
+__history = [{"date":"Wed, 14 Oct 2015 22:12:47 GMT","total":{"sloc":25326,"maintainability":1713.446167639737},"average":{"sloc":1101,"maintainability":"74.50"}}]
