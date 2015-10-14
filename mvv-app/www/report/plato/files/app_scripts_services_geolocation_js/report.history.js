fc0dcd394801d0fed6da7c886f07e6521feae581
blibla <blub@github.com>
2015-10-14 20:31:46 +0000

    Android App

--- mvv-app/www/report/plato/files/app_scripts_services_geolocation_js/report.history.js
+++ mvv-app/www/report/plato/files/app_scripts_services_geolocation_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 14 Oct 2015 04:50:43 GMT","sloc":21,"lloc":12,"functions":7,"deliveredBugs":0.1250268807750948,"maintainability":90.27989659725417,"lintErrors":7,"difficulty":6.75}]
+__history = [{"date":"Wed, 14 Oct 2015 20:31:41 GMT","sloc":21,"lloc":12,"functions":7,"deliveredBugs":0.1250268807750948,"maintainability":90.27989659725417,"lintErrors":7,"difficulty":6.75}]
--- mvv-app/www/report/plato/files/test_karma_conf_js/report.history.js
+++ mvv-app/www/report/plato/files/test_karma_conf_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 14 Oct 2015 04:50:43 GMT","sloc":77,"lloc":14,"functions":1,"deliveredBugs":0.14559225044393506,"maintainability":61.524977712198194,"lintErrors":[],"difficulty":4.295454545454546}]
+__history = [{"date":"Wed, 14 Oct 2015 20:31:41 GMT","sloc":77,"lloc":14,"functions":1,"deliveredBugs":0.14559225044393506,"maintainability":61.524977712198194,"lintErrors":[],"difficulty":4.295454545454546}]
