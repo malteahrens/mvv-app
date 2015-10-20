de0dc3a41ffe417b00e21251f4d2a2c4b09c4b14
deploy.sh <xyz@test.de>
2015-10-20 20:48:28 +0000

    publish: working on architecture
    generated from commit 062e8431bcdf6221fa7d06397e17475f0ca50c5e

--- report/plato/files/app_scripts_components_statistics_statistic_controller_js/report.history.js
+++ report/plato/files/app_scripts_components_statistics_statistic_controller_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 20 Oct 2015 04:24:03 GMT","sloc":4,"lloc":1,"functions":1,"deliveredBugs":0.025333333333333333,"maintainability":100,"lintErrors":[],"difficulty":2.1666666666666665}]
+__history = [{"date":"Tue, 20 Oct 2015 20:48:09 GMT","sloc":4,"lloc":1,"functions":1,"deliveredBugs":0.025333333333333333,"maintainability":100,"lintErrors":[],"difficulty":2.1666666666666665}]
