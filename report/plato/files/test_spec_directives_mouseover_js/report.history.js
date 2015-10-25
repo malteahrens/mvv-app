8375a9b356c568574c9ea36f49b39c3c531f7faa
deploy.sh <xyz@test.de>
2015-10-25 13:31:41 +0000

    publish: first navigation draft
    generated from commit 446e9b27a8eec489b4cb4cd748c65e513f5f9b34

--- report/plato/files/test_spec_directives_mouseover_js/report.history.js
+++ report/plato/files/test_spec_directives_mouseover_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 25 Oct 2015 12:37:19 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
+__history = [{"date":"Sun, 25 Oct 2015 13:31:41 GMT","sloc":20,"lloc":8,"functions":3,"deliveredBugs":0.05042249444035256,"maintainability":85.17094082553875,"lintErrors":8,"difficulty":3.5}]
