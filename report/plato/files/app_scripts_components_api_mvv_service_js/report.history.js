d21ae95dad2ebafc904a4a484e7fa5d085773009
deploy.sh <xyz@test.de>
2015-10-25 12:37:20 +0000

    publish: first navigation draft
    generated from commit 01b99a832fcd7974008d161a132e5a84e2b453c7

--- report/plato/files/app_scripts_components_api_mvv_service_js/report.history.js
+++ report/plato/files/app_scripts_components_api_mvv_service_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 25 Oct 2015 12:22:18 GMT","sloc":46,"lloc":31,"functions":6,"deliveredBugs":0.3915338999417981,"maintainability":70.57690470303133,"lintErrors":4,"difficulty":16.84259259259259}]
+__history = [{"date":"Sun, 25 Oct 2015 12:37:00 GMT","sloc":46,"lloc":31,"functions":6,"deliveredBugs":0.3915338999417981,"maintainability":70.57690470303133,"lintErrors":4,"difficulty":16.84259259259259}]
