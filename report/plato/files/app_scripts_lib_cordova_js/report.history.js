91899b0943c6903b9fdce1adb0e6904209264f38
deploy.sh <xyz@test.de>
2015-10-26 21:34:32 +0000

    publish: responsive layout test
    generated from commit fe29644acf960950287363aae75fa04d977f7a2e

--- report/plato/files/app_scripts_lib_cordova_js/report.history.js
+++ report/plato/files/app_scripts_lib_cordova_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 26 Oct 2015 21:12:09 GMT","sloc":224,"lloc":127,"functions":20,"deliveredBugs":1.649082750331732,"maintainability":73.22767100383267,"lintErrors":3,"difficulty":38.7}]
+__history = [{"date":"Mon, 26 Oct 2015 21:34:13 GMT","sloc":224,"lloc":127,"functions":20,"deliveredBugs":1.649082750331732,"maintainability":73.22767100383267,"lintErrors":3,"difficulty":38.7}]
--- report/plato/files/app_scripts_services_cordova_js/report.history.js
+++ report/plato/files/app_scripts_services_cordova_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 26 Oct 2015 21:12:34 GMT","sloc":16,"lloc":11,"functions":2,"deliveredBugs":0.105,"maintainability":73.97618745955141,"lintErrors":6,"difficulty":9.600000000000001}]
+__history = [{"date":"Mon, 26 Oct 2015 21:34:32 GMT","sloc":16,"lloc":11,"functions":2,"deliveredBugs":0.105,"maintainability":73.97618745955141,"lintErrors":6,"difficulty":9.600000000000001}]
