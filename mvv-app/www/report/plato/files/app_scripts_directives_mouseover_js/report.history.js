94020bf382f88b980f96a988c02fbde19e1fb70e
blibla <blub@github.com>
2015-10-27 19:45:43 +0000

    Android App

--- mvv-app/www/report/plato/files/app_scripts_directives_mouseover_js/report.history.js
+++ mvv-app/www/report/plato/files/app_scripts_directives_mouseover_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 27 Oct 2015 19:25:34 GMT","sloc":69,"lloc":24,"functions":10,"deliveredBugs":0.2666039910726603,"maintainability":83.12481278682365,"lintErrors":5,"difficulty":10.121951219512194}]
+__history = [{"date":"Tue, 27 Oct 2015 19:45:20 GMT","sloc":69,"lloc":24,"functions":10,"deliveredBugs":0.2666039910726603,"maintainability":83.12481278682365,"lintErrors":5,"difficulty":10.121951219512194}]
