752816d7864469d929138951a5b69c88962efd8a
deploy.sh <xyz@test.de>
2015-11-02 20:26:19 +0000

    publish: Delete about.html
    generated from commit e87d9aa3a0baa517ac32c0105223d9ea71d80159

--- report/plato/files/app_scripts_app_js/report.history.js
+++ report/plato/files/app_scripts_app_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 02 Nov 2015 20:24:22 GMT","sloc":57,"lloc":21,"functions":1,"deliveredBugs":0.21283997615630176,"maintainability":56.50896414430904,"lintErrors":2,"difficulty":5.76}]
+__history = [{"date":"Mon, 02 Nov 2015 20:26:00 GMT","sloc":57,"lloc":21,"functions":1,"deliveredBugs":0.21283997615630176,"maintainability":56.50896414430904,"lintErrors":2,"difficulty":5.76}]
