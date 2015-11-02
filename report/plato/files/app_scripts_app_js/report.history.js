b385066afad1cbc72371d43c6dadad00452829c1
deploy.sh <xyz@test.de>
2015-11-02 20:17:29 +0000

    publish: Update README.md
    generated from commit 9ac1e1c81607e5f2bd5395c096f81ab9be49f163

--- report/plato/files/app_scripts_app_js/report.history.js
+++ report/plato/files/app_scripts_app_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 02 Nov 2015 20:13:31 GMT","sloc":57,"lloc":21,"functions":1,"deliveredBugs":0.21283997615630176,"maintainability":56.50896414430904,"lintErrors":2,"difficulty":5.76}]
+__history = [{"date":"Mon, 02 Nov 2015 20:17:06 GMT","sloc":57,"lloc":21,"functions":1,"deliveredBugs":0.21283997615630176,"maintainability":56.50896414430904,"lintErrors":2,"difficulty":5.76}]
--- report/plato/files/test_karma_conf_js/report.history.js
+++ report/plato/files/test_karma_conf_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 02 Nov 2015 20:13:51 GMT","sloc":78,"lloc":14,"functions":1,"deliveredBugs":0.14821143267166842,"maintainability":61.49432342743258,"lintErrors":[],"difficulty":4.277777777777779}]
+__history = [{"date":"Mon, 02 Nov 2015 20:17:28 GMT","sloc":78,"lloc":14,"functions":1,"deliveredBugs":0.14821143267166842,"maintainability":61.49432342743258,"lintErrors":[],"difficulty":4.277777777777779}]
